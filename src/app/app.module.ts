b91b282a1d311f72ad895768ffdcefd35878af08
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-08-15 14:59:32 +0700

    add folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/component/sidebar/sidebar.component';
+import { LoginComponent } from './auth/login/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductFormComponent } from './pages/product/product-form/product-form.component';
+import { ProductListComponent } from './pages/product/product-list/product-list.component';
+import { CustomerFormComponent } from './pages/customer/customer-form/customer-form.component';
+import { CustomerListComponent } from './pages/customer/customer-list/customer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ProductFormComponent,
+    ProductListComponent,
+    CustomerFormComponent,
+    CustomerListComponent
   ],
   imports: [
     BrowserModule,
