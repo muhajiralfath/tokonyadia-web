ffade99db5501a85ac9efdd35d905da33a421e78
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-08-15 17:20:38 +0700

    add login & register & dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './shared/component/sidebar/sidebar.component';
-import { LoginComponent } from './auth/login/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductFormComponent } from './pages/product/product-form/product-form.component';
 import { ProductListComponent } from './pages/product/product-list/product-list.component';
 import { CustomerFormComponent } from './pages/customer/customer-form/customer-form.component';
 import { CustomerListComponent } from './pages/customer/customer-list/customer-list.component';
+import {LoginComponent} from "./auth/login/login.component";
+import { NotfoundComponent } from './shared/component/notfound/notfound.component';
+import { NavbarComponent } from './shared/component/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,9 @@ import { CustomerListComponent } from './pages/customer/customer-list/customer-l
     ProductFormComponent,
     ProductListComponent,
     CustomerFormComponent,
-    CustomerListComponent
+    CustomerListComponent,
+    NotfoundComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
